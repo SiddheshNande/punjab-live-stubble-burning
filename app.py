74b0969773553aea1949e0ec902d26579a24ef30
Siddhesh Sanjay Nande <siddheshnande4@gmail.com>
2025-11-01 19:14:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+def main():
+    st.title("Monthwise Stubble Content mapping")
+    months = ['September', 'October', 'November', 'December', 'January']
+    image_map = {
+    'September': 'images/september.jpeg',
+    'October': 'images/october.jpeg',
+    'November': 'images/november.jpeg',
+    'December': 'images/december.jpeg',
+    'January': 'images/january.jpeg'
+}
+
+    selected_month = st.select_slider(
+        "Select a month:",
+        options=months
+    )
+    if selected_month:
+        st.write(f"Displaying image for: **{selected_month}**")
+
+        image_url = image_map[selected_month]
+        st.image(image_url, caption=f"This is the image for {selected_month}.")
+
+
+if __name__ == "__main__":
+    main()
